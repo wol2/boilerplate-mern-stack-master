ee97fc0f6ec6598b2f899dead8da40c5f1b1279e
ws.park <ws.park@solarwiz.co.kr>
2020-08-21 17:31:02 +0900

    Updates

---
+++ precache-manifest.79238398c5792f515af5bed831510dd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab07496984e620db75ccff37c463a36e",
+    "url": "/boilerplate-mern-stack-master/index.html"
+  },
+  {
+    "revision": "b77ce76ea6815e2ada35",
+    "url": "/boilerplate-mern-stack-master/static/css/2.051905ae.chunk.css"
+  },
+  {
+    "revision": "57875fb55ae64dbb5880",
+    "url": "/boilerplate-mern-stack-master/static/css/main.91bf4c5a.chunk.css"
+  },
+  {
+    "revision": "b77ce76ea6815e2ada35",
+    "url": "/boilerplate-mern-stack-master/static/js/2.1df58ceb.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/boilerplate-mern-stack-master/static/js/2.1df58ceb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57875fb55ae64dbb5880",
+    "url": "/boilerplate-mern-stack-master/static/js/main.966e7f8a.chunk.js"
+  },
+  {
+    "revision": "80e585d4d08a89f2b69e",
+    "url": "/boilerplate-mern-stack-master/static/js/runtime-main.87f568d6.js"
+  }
+]);
